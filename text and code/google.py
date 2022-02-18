a9c0773724955c5a7bd0557386935d417b5b5e58
Mac Henni <cameron.reape@gmail.com>
2022-02-17 16:14:17 -0800

    code: wrote a googler

--- text and code/google.py
+++ text and code/google.py
@@ -1,5 +1,12 @@
+
 import webbrowser
-google = "https://www.google.com/search?q="
-url = google + input("what to search ")
-webbrowser.open(url,2)
-# finally!
+import asyncio
+amba = "y"
+async def lookup():
+    google = "https://www.google.com/search?q="
+    url = google + input("what to search ")
+    webbrowser.open(url,2)
+while amba == "y":
+    asyncio.run(lookup())
+    amba = input("run again? y if so, enter if not")
+    
