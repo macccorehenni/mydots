160311a36b08759b17ab1daec77a697874bc84d7
Mac Henni <cameron.reape@gmail.com>
2022-02-17 17:15:07 -0800

    VS code and code: added comments

--- text and code/google.py
+++ text and code/google.py
@@ -4,14 +4,24 @@ use it, sell it, wtf you want it!
 (In other words, this is CC-BY 0-licenced.)
  Opens Google in your default browser. Seems like it's just Chrome for me.
 Used a async, which is actually cool!'''
+#importing inmportant stuff
 import webbrowser
+#allows you to open webpages in default browser
 import asyncio
+#sets up repeat
+#just set amba (the repeat killswitch) to y
+#while doesn't run if the condition is false
 amba = "y"
 async def lookup():
+    #Next three lines actually open Google
     google = "https://www.google.com/search?q="
     url = google + input("what to search ")
+    #THIS is where Google opens
+    #url= the combined google url which is the base (var google above) with the user input (gotten by input function above)
+  
     webbrowser.open(url,2)
-while amba == "y":
+# you can repeat it, pressing enter is telling the program to quit 
+ while amba == "y":
     asyncio.run(lookup())
     amba = input("run again? y if so, enter if not ")
   #for debug
