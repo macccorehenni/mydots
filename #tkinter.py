2e500c12a5708931dcb6dd80d74dffedc667eb07
Mac Henni <cameron.reape@gmail.com>
2022-02-19 12:26:46 -0800

    tkinter.py: continued work

--- #tkinter.py
+++ #tkinter.py
@@ -1,20 +1,25 @@
 #tkinter
 from ast import Str
+from re import search
 import tkinter as tk
 import webbrowser
-def lookup(what):
+from construct import this
+def google(what):
+    
     #Next three lines actually open Google
     google = "https://www.google.com/search?q="
-    url = google + this.what
+    url = google + what
     #THIS is where Google opens
     #url= the combined google url which is the base (var google above) with the user input (gotten by input function above)
     webbrowser.open(url,2) 
-from construct import this
+
 world = tk.Tk()
 greeting = tk.Label(text="What do you want to search for?")
 greeting.pack()
 entry = tk.Entry(width=50)
 entry.pack()
-submit= tk.Button(lookup(entry.get())
+entry_text = entry.get()
+submit = tk.Button(google(entry_text))
+submit.pack()
 world.mainloop()
 
