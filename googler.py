a9f68994f7510b75d8e8badbaf6affe2b50e110d
Mac Henni <cameron.reape@gmail.com>
2022-02-19 19:09:16 -0800

    --- Auto-Git Commit ---

---
+++ googler.py
@@ -0,0 +1,29 @@
+#tkinter
+from colorsys import ONE_THIRD
+import tkinter as tk
+from turtle import heading
+import webbrowser
+
+from construct import Padding
+def google(what):
+    
+    #Next three lines actually open Google
+    google = "https://www.google.com/search?q="
+    url = google + what
+    #THIS is where Google opens
+    #url= the combined google url which is the base (var google above) with the user input (gotten by input function above)
+    webbrowser.open(url,2) 
+
+#set up for the Tkinter window
+world = tk.Tk()
+world.title("Search with Google")
+world.wm_title("Search with Google")
+greeting = tk.Label(text="What do you want to search for?",height=10)
+greeting.pack()
+entry = tk.Entry()
+entry.pack()
+entry_text = entry.get()
+submit = tk.Button(command=lambda: google(entry.get()),text="Google!")
+submit.pack()
+world.mainloop()
+
